942f01dd33bcf53d85302d8677845c65931eabfe
aziz <azizdabbou@outlook.fr>
2022-03-17 21:33:43 +0100

    second commit

---
+++ client/build/precache-manifest.b6ce38b9dbacfa5506ad9059da556915.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4347b6846f8f01d5e540f378047b812f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca382a2336ac4d8f0077",
+    "url": "/static/css/2.5544c887.chunk.css"
+  },
+  {
+    "revision": "a9a43e29af27f3f3e8f7",
+    "url": "/static/css/main.0e361bf5.chunk.css"
+  },
+  {
+    "revision": "ca382a2336ac4d8f0077",
+    "url": "/static/js/2.6f689132.chunk.js"
+  },
+  {
+    "revision": "8f448bce97d01acd647a1d4bc48482b1",
+    "url": "/static/js/2.6f689132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a43e29af27f3f3e8f7",
+    "url": "/static/js/main.8767f380.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "d0558d91063038236b60e3ef71fdc1fd",
+    "url": "/static/media/Download_on_the_App_Store_Badge (2).d0558d91.svg"
+  },
+  {
+    "revision": "57b01d25df187da54592b52a064ee2f1",
+    "url": "/static/media/Google_Play_Store_badge_FR.57b01d25.svg"
+  },
+  {
+    "revision": "5645d2bc5130c13ffe48346b40e85d7a",
+    "url": "/static/media/our-team.5645d2bc.jpg"
+  },
+  {
+    "revision": "d3f3293c96d45ad342fc02c01074288f",
+    "url": "/static/media/siteLogo.d3f3293c.jpg"
+  }
+]);
